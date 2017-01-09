8d35c7c234ccd31cc875eaabbdce05d41b40c644
Artur Yakimovich, PhD <a.yakimovich@ucl.ac.uk>
2017-01-09 23:37:16 +0000

    added working spinner

--- js/uViewer.js
+++ js/uViewer.js
@@ -1,6 +1,34 @@
 function uViewer() {
+  var opts = {
+      lines: 13 // The number of lines to draw
+    , length: 56 // The length of each line
+    , width: 14 // The line thickness
+    , radius: 69 // The radius of the inner circle
+    , scale: 1 // Scales overall size of the spinner
+    , corners: 1 // Corner roundness (0..1)
+    , color: '#ccc' // #rgb or #rrggbb or array of colors
+    , opacity: 0.75 // Opacity of the lines
+    , rotate: 16 // The rotation offset
+    , direction: 1 // 1: clockwise, -1: counterclockwise
+    , speed: 1 // Rounds per second
+    , trail: 87 // Afterglow percentage
+    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+    , zIndex: 2e9 // The z-index (defaults to 2000000000)
+    , className: 'spinner' // The CSS class to assign to the spinner
+    , top: '170%' // Top position relative to parent
+    , left: '50%' // Left position relative to parent
+    , shadow: false // Whether to render a shadow
+    , hwaccel: false // Whether to use hardware acceleration
+    , position: 'absolute' // Element positioning
+    }
+
+
 
   initialize();
+  var target = document.getElementById("svgDiv");
+  var spinner = new Spinner(opts).spin();
+  target.appendChild(spinner.el);
+
   console.log("uViewer initialized");
   var imageFolder = document.getElementById('imageFolderPath').value,
       rgbImageFileName = document.getElementById('rgbImageName').value,
@@ -13,48 +41,37 @@ function uViewer() {
   document.getElementById('imageFolderPath').value = imageFolder;
   document.getElementById('rgbImageName').value = rgbImageFileName;
 
-  
-  var opts = {
-    lines: 9, // The number of lines to draw
-    length: 9, // The length of each line
-    width: 5, // The line thickness
-    radius: 14, // The radius of the inner circle
-    color: '#EE3124', // #rgb or #rrggbb or array of colors
-    speed: 1.9, // Rounds per second
-    trail: 40, // Afterglow percentage
-    className: 'spinner', // The CSS class to assign to the spinner
-  };
-
 
 
 
 //prealod all images before starting
 var rgbImage = new Image();
-var spinner = new Spinner(opts).spin(rgbImage);
+
 rgbImage.src = imageFolder+rgbImageFileName;
 rgbImage.addEventListener("load", function() {
   console.log("rgb image loaded "+rgbImage.src);
   var redImage = new Image();
-  var spinner = new Spinner(opts).spin(redImage);
+
   redImage.src = imageFolder+redImageFileName;
   redImage.addEventListener("load", function() {
     console.log("red image loaded "+redImage.src);
     var greenImage = new Image();
-    var spinner = new Spinner(opts).spin(greenImage);
+
     greenImage.src = imageFolder+greenImageFileName;
     greenImage.addEventListener("load", function() {
       console.log("green image loaded "+greenImage.src);
       var blueImage = new Image();
-      var spinner = new Spinner(opts).spin(blueImage);
+
       blueImage.src = imageFolder+blueImageFileName;
       blueImage.addEventListener("load", function() {
         console.log("blue image loaded "+blueImage.src);
         var grayImage = new Image();
-        var spinner = new Spinner(opts).spin(blueImage);
+
         grayImage.src = imageFolder+grayImageFileName;
         grayImage.addEventListener("load", function() {
           console.log("gray image loaded "+grayImage.src);
           console.log("Initializing main svg");
+          spinner.stop();
           mainSvg(rgbImage, redImage, greenImage, blueImage, grayImage);
         });
       });
